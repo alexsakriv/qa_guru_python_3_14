878bce42d351fb05c20020b75ee0f788b22fb33f
krivorucenkoaleksandra <alexsakriv@gmail.com>
2023-04-06 16:15:10 +0300

    добавила вебдрайвер в конфтест

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,10 @@ import os
 import pytest
 from dotenv import load_dotenv
 from qa_guru_python_3_14.utils.base_session import BaseSession
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+from selene.support.shared import browser
+
 
 load_dotenv()
 
@@ -10,6 +14,23 @@ load_dotenv()
 @pytest.fixture(scope="session")
 def demowebshop():
     api_url = os.getenv("API_URL")
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+
+    driver = webdriver.Remote(
+        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options)
+
+    browser.config.driver = driver
+
     return BaseSession(api_url)
 
 
